18b97e64fb62a416db9be2d62c23dc89050cfa65
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-10-07 16:54:05 +0700

    Add files via upload

---
+++ Bai2/bai2-3.py
@@ -0,0 +1,7 @@
+print("Sinh vien: Hoang Vo Khac Quan")
+print("Ma so sv: 245752021610150")
+n=int(input("Enter a number---->"))
+if n % 2 == 0:
+    print ("EVEN Number")
+else:
+    print ("ODD Number")
