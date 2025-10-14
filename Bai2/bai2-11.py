5245f9c8a686fc66021da75ad00c3992f4e6e981
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-10-14 16:08:49 +0700

    Add files via upload

---
+++ Bai2/bai2-11.py
@@ -0,0 +1,10 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+l=[1, "python", 4 ,7]
+k=["cse", 2,"guntur",8]
+m=[]
+m.append(1)
+m.append(k)
+print(m)
+d={1:l,2:k,"combin_list":m}
+print(d)
