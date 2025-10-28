1470b1bb2c2119076180e24239349ef931959461
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-10-28 14:29:08 +0700

    Add files via upload

---
+++ Bai3/bai3_7.py
@@ -0,0 +1,8 @@
+print("Sinh vien: Hoang Vo Khac Quan")
+print("Ma so sv: 245752021610150")
+def checkValue(n):
+ if n%2 == 0:
+     print ("Đây là một số chẵn")
+ else:
+     print ("Đây là một số lẻ")
+checkValue(8)
