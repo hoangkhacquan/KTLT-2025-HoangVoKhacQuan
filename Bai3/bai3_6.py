6568b7fcb98b6cdc2b492dd533a20a0b1b957671
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-10-28 14:28:02 +0700

    Add files via upload

---
+++ Bai3/bai3_6.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Hoang Vo Khac Quan")
+print("Ma so sv: 245752021610150")
+def get_sum(*num):
+ tmp = 0
+ for i in num:
+     tmp += i
+ return tmp
+result = get_sum(1, 2, 3, 4, 5)
+print(result)
