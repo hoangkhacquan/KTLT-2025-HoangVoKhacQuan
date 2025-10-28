b7eb1f3fd676c6f3902c0bb9909d92d343d243e0
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-10-28 16:10:52 +0700

    Add files via upload

---
+++ Bai4/bai4_5.py
@@ -0,0 +1,7 @@
+print("Sinh vien: Hoang Vo Khac Quan")
+print("Ma so sv: 245752021610150")
+ds = input("Nhập danh sách từ: ").split()
+ds.reverse()
+print("Danh sách theo thứ tự ngược lại:")
+for tu in ds:
+    print(tu, end=" ") 
