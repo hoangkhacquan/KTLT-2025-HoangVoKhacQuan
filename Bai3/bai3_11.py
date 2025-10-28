753fb0edd83c867b6583cac8c8368f16b5437e96
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-10-28 14:56:51 +0700

    Add files via upload

---
+++ Bai3/bai3_11.py
@@ -0,0 +1,10 @@
+print("Sinh vien: Hoang Vo Khac Quan")
+print("Ma so sv: 245752021610150")
+def benefit(P, n):
+    rate = 0.04
+    A = P * (1 + rate) ** n
+    return A
+P = float(input("Nhập số tiền ban đầu: "))
+n = int(input("Nhập số năm gửi: "))
+tien_nhan_duoc = benefit(P, n)
+print("Số tiền nhận được sau", n, "năm là:", round(tien_nhan_duoc, 2))
