2b3caa3b7fe8bca28a875697499b524ed6d9c302
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-11-04 14:04:09 +0700

    Add files via upload

---
+++ Bai4/bai4_13.py
@@ -0,0 +1,6 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+ds = ['123', 'abc', 'xyz', '456']
+print("Danh sách ban đầu:", ds)
+vitri = ds.index('abc')
+print("Vị trí của chuỗi 'abc' là:", vitri)
