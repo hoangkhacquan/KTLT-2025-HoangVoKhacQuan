b715209e01d10e5a54feed1ce33db64deb4b10ea
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-11-04 14:03:19 +0700

    Add files via upload

---
+++ Bai4/bai4_12.py
@@ -0,0 +1,9 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+ds = ['123', 'abc', 'xyz', '456']
+print("Danh sách ban đầu:", ds)
+ds.remove('123')
+print("Danh sách sau khi xóa phần tử '123':")
+for ch in ds:
+    print(ch)
+
