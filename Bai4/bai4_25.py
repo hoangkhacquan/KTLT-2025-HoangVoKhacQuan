4a532e42b80de8c691013878b009587f2d3c4599
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-11-04 15:32:29 +0700

    Add files via upload

---
+++ Bai4/bai4_25.py
@@ -0,0 +1,6 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+s = input("Nhập danh sách số, cách nhau bởi dấu phẩy: ")
+numbers = [int(x) for x in s.split(",")]
+odd_numbers = [x for x in numbers if x % 2 != 0]
+print("Các số lẻ là:", odd_numbers)
