7fb440faff311a7b3085f56e65a944fbc4181550
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-11-04 14:07:33 +0700

    Add files via upload

---
+++ Bai4/bai4_15.py
@@ -0,0 +1,8 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+chuoi = input("Nhập chuỗi các từ tiếng Anh, cách nhau bằng dấu cách: ")
+ds_tu = chuoi.split()
+ds_tu.sort()
+print("Các từ sau khi sắp xếp theo thứ tự từ điển là:")
+for tu in ds_tu:
+    print(tu)
