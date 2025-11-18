6dea82a926c941fdc17cc5b778e2733c1ce6e3da
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-11-18 13:58:35 +0700

    Add files via upload

---
+++ Bai6/bai6.2.py
@@ -0,0 +1,10 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+class Hinhchunhat:
+    def __init__(self, dai, rong):
+        self.dai = dai
+        self.rong = rong
+    def dientich(self):
+        return self.dai * self.rong
+hcn = Hinhchunhat(5, 3)
+print("Diện tích hình chữ nhật là:", hcn.dientich())
