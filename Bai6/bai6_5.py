f4eaa60dd871a99f1a32288ce2e952fcc80059f0
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-11-18 14:25:29 +0700

    Add files via upload

---
+++ Bai6/bai6_5.py
@@ -0,0 +1,7 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+class py_solution:
+    def reverse_words(self, s):
+        return ' '.join(reversed(s.split()))
+sol = py_solution()
+print(sol.reverse_words("hello .py"))   
