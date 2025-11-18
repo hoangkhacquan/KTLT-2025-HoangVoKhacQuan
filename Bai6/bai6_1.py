1838b223e049e8b1094ff220e561963fbc9061bf
hoangkhacquan <hoangkhacquan2510@gmail.com>
2025-11-18 13:50:51 +0700

    Add files via upload

---
+++ Bai6/bai6_1.py
@@ -0,0 +1,9 @@
+print ("Sinh vien: Hoang Vo Khac Quan")
+print ("Ma so sv: 245752021610150")
+class Circle(object):
+    def __init__(self, r):
+        self.radius = r
+    def area(self):
+         return self.radius**2*3.14
+aCircle = Circle(2)
+print (aCircle.area())
